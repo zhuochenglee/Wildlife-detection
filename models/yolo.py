fa8ec5dd7d57ffa11009acaafc79195da8816479
zhuochenglee <1151246887@qq.com>
2023-07-27 14:09:18 +0800

    删除yolo.py中的Conv_ATT等

--- models/yolo.py
+++ models/yolo.py
@@ -760,7 +760,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                  Ghost, GhostCSPA, GhostCSPB, GhostCSPC,
                  SwinTransformerBlock, STCSPA, STCSPB, STCSPC,
                  SwinTransformer2Block, ST2CSPA, ST2CSPB, ST2CSPC,
-                 Conv_ATT, SPPCSPC_ATT]:
+                 ]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
